c2a4a1ed98fe4ff64ce822fb807b607576708b76
chickenONsteak <austinchenwd@gmail.com>
2025-07-13 22:55:32 +0800

    shuffle

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-import { allTiles } from "./tiles";
+import { allTiles } from "./tiles.js";
+
+// use Durstenfeld's version of the Fisher-Yates shuffle
+function shuffle(fullSetOfTiles) {
+  // note: the condition for the FOR loop to stop = unshuffledIndex > 0
+  // this is because for e.g. when there are only 2 items left unshuffled, we will get unshuffledIndex = 1 ->
+  // this means that when swapping, the 2nd tile (the last unshuffled tile) will be swapped with a random tile — which can be the 1st or 2nd tile (itself)
+  for (
+    let unshuffledIndex = fullSetOfTiles.length - 1;
+    unshuffledIndex > 0;
+    unshuffledIndex--
+  ) {
+    let randomIndex = Math.floor(Math.random() * (unshuffledIndex + 1));
+    [fullSetOfTiles[unshuffledIndex], fullSetOfTiles[randomIndex]] = [
+      fullSetOfTiles[randomIndex],
+      fullSetOfTiles[unshuffledIndex],
+    ];
+  }
+  return fullSetOfTiles;
+}
+
+shuffle(allTiles);
+console.log(allTiles);
--- js/tiles.js
+++ js/tiles.js
@@ -19,15 +19,18 @@ function createTiles(variations, type, numCopies) {
 
 const allTiles = [];
 const characters = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const dragon = ["hong zhong", "bai ban", "fa cai"];
-const animal = ["cat", "mouse", "rooster", "centipede"];
+const winds = ["dong", "nan", "xi", "bei"];
+const dragons = ["hong zhong", "bai ban", "fa cai"];
+const animals = ["cat", "mouse", "rooster", "centipede"];
 
 allTiles.push(
   ...createTiles(characters, "suo", 4),
   ...createTiles(characters, "tong", 4),
   ...createTiles(characters, "wan", 4),
-  ...createTiles(dragon, "dragon", 4),
-  ...createTiles(animal, "animal", 1)
+  ...createTiles(winds, "wind", 4),
+  ...createTiles(dragons, "dragon", 4),
+  ...createTiles(animals, "animal", 1)
 );
 
+// note: there are 140 tiles in total here (148 - 8 flowers)
 export { allTiles };
