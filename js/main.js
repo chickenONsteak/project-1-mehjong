3d1faf837f75e287b1c61b2268ea9a2a0752d7ba
chickenONsteak <austinchenwd@gmail.com>
2025-07-15 18:40:52 +0800

    jia lat

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-import { allTiles, characters, winds, dragons, animals } from "./tiles.js";
+import {
+  allTiles,
+  suoZi,
+  tongZi,
+  wanZi,
+  winds,
+  dragons,
+  animals,
+} from "./tiles.js";
 
 const players = ["Maddy", "Yen", "Yi Lin", "Shuwei"]; // CONTAINS: consolidated player names
 const playerDetails = []; // CONTAINS: player order, player names, and assigned wind
@@ -8,6 +16,7 @@ function rollDice() {
   return Math.ceil(Math.random() * 6);
 }
 
+// ASSIGN WINDS ACCORDING TO WHO GOES FIRST ETC
 function assignWinds() {
   // FIRSTLY: sort the dice rolls
   // the human player is always the 1st roller
@@ -32,6 +41,7 @@ function assignWinds() {
       playerName: players[i],
       assignedWind: winds[i],
       tilesInHand: [],
+      tilesOutsideHand: [],
       tai: 0,
     };
     playerDetails.push(obj);
@@ -60,19 +70,23 @@ function shuffle(fullSetOfTiles) {
 
 // DISTRIBUTING THE TILES
 function distributeTiles(undrawnTiles) {
-  let lastPlayerHandTiles = playerDetails[playerDetails.length - 1].tilesInHand;
+  let lastPlayerHandTiles = playerDetails[playerDetails.length - 1].tilesInHand; // we want the distribution to stop once the last player has drawn his/her tiles
   while (lastPlayerHandTiles.length < 13) {
+    // players draw 4 tiles at a time until they have 12 tiles
     if (lastPlayerHandTiles.length < 12) {
       for (let i = 0; i < playerDetails.length; i++) {
+        // move into player's hands
         playerDetails[i].tilesInHand.push(
           undrawnTiles[0],
           undrawnTiles[1],
           undrawnTiles[2],
           undrawnTiles[3]
         );
+        // remove from undistributed tiles
         undrawnTiles.splice(0, 4);
       }
     } else if (lastPlayerHandTiles.length === 12) {
+      // once the last player draws his 12th tile, the 1st player draws 2 tiles while others draw 1 tile each
       for (let i = 0; i < playerDetails.length; i++) {
         if (i === 0) {
           playerDetails[i].tilesInHand.push(undrawnTiles[0], undrawnTiles[1]);
@@ -84,10 +98,74 @@ function distributeTiles(undrawnTiles) {
       }
     }
   }
+  // update tile property (distributed) to true
+  for (let i = 0; i < playerDetails.length; i++) {
+    for (let j = 0; j < playerDetails[i].tilesInHand.length; j++) {
+      playerDetails[i].tilesInHand[j].distributed = true;
+    }
+  }
+  return undrawnTiles;
 }
 
+// CHECK FOR SPECIAL TILES IN HAND LIKE ANIMAL TILES
+function takeReplacementTiles(undrawnTiles) {
+  let needToBu = true;
+  do {
+    needToBu = false; // assume no need to "bu" first
+    for (let i = 0; i < playerDetails.length; i++) {
+      const currentPlayer = playerDetails[i];
+      // total sum of special tiles held by all players — will act as a factor for the switch
+      // note: this resets back to 0 after all 4 players have finished their round of "bu"
+      let specialTileCount = 0;
+      console.log(currentPlayer.tilesInHand);
+      // STEP 1: check player by player for special tiles and push them outside of hand
+      // note: loop from the back to prevent skipping over tiles when index shifts
+      for (let j = currentPlayer.tilesInHand.length - 1; j < 0; j--) {
+        // if there are special tiles (animal tiles are 51, 52, 53, and 54), push them to outside of the hand
+        if (currentPlayer.tilesInHand[j].tileId >= 51) {
+          // update tile property (faceUp) to true
+          currentPlayer.tilesInHand[j].faceUp = true;
+          // move from hand to outside of hand, facing up
+          currentPlayer.tilesOutsideHand.push(currentPlayer.tilesInHand[j]);
+          currentPlayer.tilesInHand.splice(j, 1);
+          // STEP 2: draw the num of tiles pushed out of hand from the end of the undistributed tile
+          const buTile = undrawnTiles.pop();
+          buTile.distributed = true;
+          buTile.faceUp = true;
+          currentPlayer.tilesInHand.push(buTile);
+          specialTileCount++;
+        }
+        console.log(j);
+      }
+      // STEP 3: flip back the switch if any of the players had a special tile
+      // this is because there is a chance that the "bu" tile might be a special tile again
+      if (specialTileCount > 0) {
+        needToBu = true;
+      }
+    }
+  } while (needToBu);
+  return undrawnTiles;
+}
+
+function sortHand(playerInfo) {
+  playerInfo.tilesInHand.sort((a, b) => a.tileId - b.tileId);
+}
+
+// function checkWinner {
+
+// }
+
 assignWinds();
-console.log(playerDetails);
-// console.log(allTiles);
-distributeTiles(shuffle(allTiles));
-console.log(playerDetails[0].tilesInHand);
+// console.log(playerDetails);
+const shuffledTiles = shuffle(allTiles);
+let undistributedTiles = distributeTiles(shuffledTiles);
+for (let i = 0; i < playerDetails.length; i++) {
+  sortHand(playerDetails[i]);
+  //   console.log(playerDetails[i].tilesInHand);
+}
+undistributedTiles = takeReplacementTiles(undistributedTiles);
+// console.log(undistributedTiles);
+// for (let i = 0; i < playerDetails.length; i++) {
+//   console.log(playerDetails[i].tilesInHand);
+//   console.log(playerDetails[i].tilesOutsideHand);
+// }
--- js/tiles.js
+++ js/tiles.js
@@ -1,18 +1,17 @@
 // Create classes to generate tiles
 class GenerateBaseTiles {
-  constructor(value, suit, distributed = false, faceUp = false) {
-    this.value = value;
-    this.suit = suit;
+  constructor(tileId, distributed = false, faceUp = false) {
+    this.tileId = tileId;
     this.distributed = distributed;
     this.faceUp = faceUp;
   }
 }
 
-function createTiles(variations, type, numCopies) {
+function createTiles(tile, numCopies) {
   const tiles = [];
-  for (let i = 0; i < variations.length; i++) {
+  for (let i = 0; i < tile.length; i++) {
     for (let j = 0; j < numCopies; j++) {
-      const newTile = new GenerateBaseTiles(variations[i], type);
+      const newTile = new GenerateBaseTiles(tile[i]);
       tiles.push(newTile);
     }
   }
@@ -20,19 +19,21 @@ function createTiles(variations, type, numCopies) {
 }
 
 const allTiles = [];
-const characters = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const winds = ["dong", "nan", "xi", "bei"];
-const dragons = ["hong zhong", "bai ban", "fa cai"];
-const animals = ["cat", "mouse", "rooster", "centipede"];
+const suoZi = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // yi suo, er suo, ...
+const tongZi = [11, 12, 13, 14, 15, 16, 17, 18, 19]; // yi tong, er tong, ...
+const wanZi = [21, 22, 23, 24, 25, 26, 27, 28, 29]; // yi wan, er wan, ...
+const winds = [31, 32, 33, 34]; // dong, nan, xi, bei
+const dragons = [41, 42, 43]; // hong zhong, bai ban, fa cai
+const animals = [51, 52, 53, 54]; // cat, mouse, rooster, centipede
 
 allTiles.push(
-  ...createTiles(characters, "suo", 4),
-  ...createTiles(characters, "tong", 4),
-  ...createTiles(characters, "wan", 4),
-  ...createTiles(winds, "wind", 4),
-  ...createTiles(dragons, "dragon", 4),
-  ...createTiles(animals, "animal", 1)
+  ...createTiles(suoZi, 4),
+  ...createTiles(tongZi, 4),
+  ...createTiles(wanZi, 4),
+  ...createTiles(winds, 4),
+  ...createTiles(dragons, 4),
+  ...createTiles(animals, 1)
 );
 
 // note: there are 140 tiles in total here (148 - 8 flowers)
-export { allTiles, characters, winds, dragons, animals };
+export { allTiles, suoZi, tongZi, wanZi, winds, dragons, animals };
